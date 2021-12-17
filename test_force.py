b35866a58f466841d6c5f2ef87d69d565fbf3e43
Hidgik <Egor-655@yandex.ru>
2021-12-17 23:56:12 +0500

    Что есть..

--- test_force.py
+++ test_force.py
@@ -1,24 +1,11 @@
 from math import cos, sin
-import pygame as pg
-from random import randrange
+import pygame
+from random import randrange, triangular
 from pygame.event import event_name
 import pymunk.pygame_util
 import pymunk
-
-pymunk.pygame_util.positive_y_is_up = False
-
-RES = WIDTH, HEIGHT = 2000, 2000
-FPS = 60
-
-
-pg.init()
-surface = pg.display.set_mode((900, 720))
-clock = pg.time.Clock()
-draw_options = pymunk.pygame_util.DrawOptions(surface)
-
-space = pymunk.Space()
-space.gravity = 0, 0
-
+import os
+import sys
 
 def draw_wall(x, y, x2, y2):
     c = [[x, y, x2, y], [x2, y, x2, y2], [x2, y2, x, y2], [x, y2, x, y]]
@@ -28,95 +15,120 @@ def draw_wall(x, y, x2, y2):
         space.add(segment_shape)
         segment_shape.elasticity = 0.8
         segment_shape.friction = 1.0
+    return space
+
+class Boat:
+    def __init__(self):
+        self.fps = 60
+        car_mass = 0.5
+        self.old_position_x = self.old_position_y = 100
+        self.turn, self.move = 0, 0
+        self.translation = pymunk.Transform()
+
+        self.car_shape = pymunk.Poly.create_box(None, size=(100, 60))
+        self.car_shape.color = [randrange(256) for i in range(4)]
+        car_moment = pymunk.moment_for_poly(car_mass/5, self.car_shape.get_vertices())
+        self.car_shape.body = pymunk.Body(car_mass, car_moment)
+        self.car_shape.body.position = (100, 100)
+        self.car_shape.body.angle = -1
+        space.add(self.car_shape, self.car_shape.body)
 
 
-draw_wall(0, 0, WIDTH, HEIGHT)
-draw_wall(WIDTH / 3, HEIGHT / 3, WIDTH - WIDTH / 3, HEIGHT - HEIGHT / 3)
-car_mass = 0.5
+class Player_Boat(Boat):
+    def __init__(self, left, right, up, down):
+        super().__init__()
+        self.left, self.right, self.up, self.down = left, right, up, down
+    
+    def update(self, event):
+        angularForce = 1*self.car_shape.body.angular_velocity
+        self.car_shape.body.apply_force_at_local_point((0, angularForce), (-50, 0))
+        self.car_shape.body.apply_force_at_local_point((0, -angularForce), (50, 0))
+        # компенсация заноса
+        angle = self.car_shape.body.angle
+        velocity = self.car_shape.body.velocity.rotated(-angle)
+        self.car_shape.body.apply_force_at_local_point((0, 1*-velocity.y))
+        # естественное торможение
+        self.car_shape.body.apply_force_at_local_point((0.1*-velocity.x, 0))
+        # мотор
+        self.car_shape.body.apply_force_at_local_point((10*self.move, 3*self.turn), (-50, 0))
+        space.step(1 / self.fps)
+        space.debug_draw(draw_options)
+        if event.type == pygame.KEYDOWN:
+            if event.key == self.left:
+                self.turn = -1
+            elif event.key == self.right:
+                self.turn = 1
+            elif event.key == self.up:
+                self.move = 1
+            elif event.key == self.down:
+                self.move = -1
+        if event.type == pygame.TEXTINPUT:
+            if event.text == self.left:
+                self.turn = -1
+            elif event.text == self.right:
+                self.turn = 1
+            elif event.text == self.up:
+                self.move = 1
+            elif event.text == self.down:
+                self.move = -1
+        if event.type == pygame.KEYUP:
+            if event.key in [self.left, self.right]:
+                self.turn = 0
+            if event.key in [self.up, self.down]:
+                self.move = 0
+        self.translation = self.translation.translated(
+            self.old_position_x - self.car_shape.body.position.x,
+            self.old_position_y - self.car_shape.body.position.y,
+        )
+        self.old_position_x = self.car_shape.body.position.x
+        self.old_position_y = self.car_shape.body.position.y
 
-car_shape = pymunk.Poly.create_box(None, size=(100, 60))
-car_shape.color = [randrange(256) for i in range(4)]
-car_moment = pymunk.moment_for_poly(car_mass/5, car_shape.get_vertices())
-car_shape.body = pymunk.Body(car_mass, car_moment)
-car_shape.body.position = (100, 100)
-car_shape.body.angle = -1
-space.add(car_shape, car_shape.body)
+    def camera(self):
+        # зум камеры
+        scaling = 1
 
-RIGHT = "to the right"
-LEFT = "to the left"
-STOP = "stop"   
-UP = "up"
-DOWN = "down"
+        draw_options.transform = (
+            pymunk.Transform.translation(300, 300)
+            @ pymunk.Transform.scaling(scaling)
+            @ self.translation
+            @ pymunk.Transform.rotation(0)
+            @ pymunk.Transform.translation(-300, -300)
+        )
 
-motion = STOP
-motion2 = STOP
+def main():
+    pymunk.pygame_util.positive_y_is_up = False
 
-translation = pymunk.Transform()
-scaling = 1
+    RES = WIDTH, HEIGHT = 2000, 2000
+    FPS = 60
 
-old_position_x = old_position_y = 100
+    pygame.init()
+    surface = pygame.display.set_mode((900, 720))
+    clock = pygame.time.Clock()
+    global draw_options
+    draw_options = pymunk.pygame_util.DrawOptions(surface)
 
-turn, move = 0, 0
-while True:
-    pg.display.flip()
-    clock.tick(FPS)
+    global space
+    space = pymunk.Space()
+    space.gravity = 0, 0
 
-    surface.fill(pg.Color('black'))
+    space = draw_wall(0, 0, WIDTH, HEIGHT)
+    space = draw_wall(WIDTH / 3, HEIGHT / 3, WIDTH - WIDTH / 3, HEIGHT - HEIGHT / 3)
 
-# компенсация вращения
-    angularForce = 1*car_shape.body.angular_velocity
-    car_shape.body.apply_force_at_local_point((0, angularForce), (-50, 0))
-    car_shape.body.apply_force_at_local_point((0, -angularForce), (50, 0))
-# компенсация заноса
-    angle = car_shape.body.angle
-    velocity = car_shape.body.velocity.rotated(-angle)
-    car_shape.body.apply_force_at_local_point((0, 1*-velocity.y))
-# естественное торможение
-    car_shape.body.apply_force_at_local_point((0.1*-velocity.x, 0))
-# мотор
-    car_shape.body.apply_force_at_local_point((10*move, 3*turn), (-50, 0))
-    space.step(1 / FPS)
-    space.debug_draw(draw_options)
+    running = True
 
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            exit()
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_LEFT:
-                motion = LEFT
-                turn = -1
-            elif event.key == pg.K_RIGHT:
-                motion = RIGHT
-                turn = 1
-            elif event.key == pg.K_UP:
-                motion = UP
-                move = 1
-            elif event.key == pg.K_DOWN:
-                motion2 = DOWN
-                move = -1
-        elif event.type == pg.KEYUP:
-            if event.key in [pg.K_LEFT, pg.K_RIGHT]:
-                motion = STOP
-                turn = 0
-            if event.key in [pg.K_UP, pg.K_DOWN]:
-                motion2 = STOP
-                move = 0
-
-# позиция камеры
-    translation = translation.translated(
-        old_position_x - car_shape.body.position.x,
-        old_position_y - car_shape.body.position.y,
-    )
-    old_position_x = car_shape.body.position.x
-    old_position_y = car_shape.body.position.y
-
-# зум камеры
-    scaling = 1
-
-    draw_options.transform = (
-        pymunk.Transform.translation(300, 300)
-        @ pymunk.Transform.scaling(scaling)
-        @ translation
-        @ pymunk.Transform.rotation(0)
-        @ pymunk.Transform.translation(-300, -300)
-    )
+    c = Player_Boat(pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN)
+    c2 = Player_Boat('a', 'd', 'w', 's')
+    while running:
+        pygame.display.flip()
+        clock.tick(FPS)
+        surface.fill(pygame.Color('black'))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        c.update(event)
+        c2.update(event)
+        c.camera()
+        
+    pygame.quit()
+if __name__ == '__main__':
+    main()
