685af17dfa9277dc0d2cbfe2e96f336c86ffe2e1
Hidgik <Egor-655@yandex.ru>
2022-01-20 21:03:29 +0500

    Новые классы

--- config.py
+++ config.py
@@ -7,3 +7,18 @@ class Collisiontypes:
 class Screen:
     WIDTH = 920
     HEIGHT = 700
+
+class Specifications:
+    BOATS = [[0.5, "yacht_1.png", 150, 0.01, 0.005],
+             [0.5, "yacht_2.png", 150, 0.01, 0.005],
+             [0.5, "yacht_3.png", 150, 0.01, 0.005],
+             [0.5, "yacht_4.png", 150, 0.01, 0.005],
+             [0.5, "yacht_5.png", 150, 0.01, 0.005],
+             [0.5, "yacht_6.png", 150, 0.01, 0.005]]
+
+class Tracks:
+    TRACKS = [["Track_1.jpg", (23, 23, 27, 27, 14)],
+             ["Track_2.jpg", (24, 24, 29, 29, 18)],
+             ["Track_3.jpg", (25, 25, 30, 30, 36)],
+             ["Track_4.jpg", (15, 15, 19, 19, 3)]]
+
