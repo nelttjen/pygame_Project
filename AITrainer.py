6b70552df259c9cdee8dc2943ae27deefa3cda7d
Hidgik <Egor-655@yandex.ru>
2022-01-20 21:01:59 +0500

    Связь стартового экрана, игры и таблицы рекордов

--- AITrainer.py
+++ AITrainer.py
@@ -11,6 +11,12 @@ from Boat.levelBuilder2 import SandBox2
 from Boat.levelBuilder3 import SandBox3
 from Boat.RadarManager import RadarManager
 from Config import Collisiontypes
+from Config import Specifications
+from Boat.mapGenerator import MapGenerator
+from Config import Tracks
+from startscreen import Startscreen
+from table_records import Table_records
+
 
 class Main:
     def __init__(self, w, h, GAME_FPS):
@@ -18,24 +24,39 @@ class Main:
         self.size = self.w, self.h
 
     def run_game(self, is_debug=False):
+        start = Startscreen()
+        start.start()
+        while start.res[0] == 'records':
+            try:
+                table = Table_records()
+                table.start()
+            except Exception:
+                pass
+            start.start()
         pg.init()
         pymunk.pygame_util.positive_y_is_up = False
         space = pymunk.Space()
         radarManager = RadarManager(space, Collisiontypes.SENSOR)
         surface = pg.display.set_mode((self.w, self.h))
         level = SandBox3()
-        level.build(space, (100, 73))
+        #level.build(space, (100, 73), MapGenerator(23, 23, 27, 27, 14))  
+        #level.build(space, (100, 73), MapGenerator(14, 14, 18, 18, 12))
 
+        #print(Tracks.TRACKS[start.res[0] - 1][1])
+        level.build(space, (100, 73), MapGenerator(*Tracks.TRACKS[start.res[0] - 1][1]))#MapGenerator(30, 30, 35, 35, 22))
+        #level.build(space, (100, 73), MapGenerator(24, 24, 29, 29, 18))
+        #level.build(space, (100, 73), MapGenerator(25, 25, 30, 30, 36))
+        #level.build(space, (100, 73), MapGenerator(15, 15, 19, 19, 3))
         boats = [
-            BaseBoat(space, radarManager, (0.5, "yacht.png", 150, 0.01, 0.005), level),
-            BaseBoat(space, radarManager, (0.5, "yacht.png", 150, 0.01, 0.005), level),
+            BaseBoat(space, radarManager, (Specifications.BOATS[start.res[1] - 1]), level),
+            BaseBoat(space, radarManager, (Specifications.BOATS[3]), level),
             #BaseBoat(space, radarManager, (0.5, "yacht.png", 150, 0.01, 0.005), level)
         ]
         controllers = [
             #SimpleController(boats[0], level),
             SimpleController(boats[1], level),
             KeyboardController(boats[0], level, pg.K_LEFT, pg.K_RIGHT, pg.K_UP, pg.K_DOWN),
-#            KeyboardController(boats[1], "a", "d", "w", "s")
+            #KeyboardController(boats[1], level, "a", "d", "w", "s")
         ]
 
         radarManager.registerCollisionType(Collisiontypes.BOAT)
@@ -52,7 +73,7 @@ if __name__ == '__main__':
     FPS = 60  
     DEBUG = False
 
-    SIZE = WIGHT, HEIGHT = 900, 720
+    SIZE = WIGHT, HEIGHT = 920, 700
     app = Main(WIGHT, HEIGHT, FPS)
 
     code = app.run_game(DEBUG)
